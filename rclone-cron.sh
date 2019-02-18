510118743cfafb342de59edc6f53d6c36e423d87
Eduardo Campacci <campacc@gmail.com>
2019-02-18 13:34:27 -0300

    Ajustado caminho de destino do rclone

--- rclone-cron.sh
+++ rclone-cron.sh
@@ -2,6 +2,9 @@
 
 if pidof -o %PPID -x “rclone-cron.sh”; then
 exit 1
+
 fi
-rclone sync -P --bwlimit=0.375M /home/eduardo/Documentos/rclone/ dropbox_hw:hewitt_equipamentos/arquivos_osasco/
+# -P é para exibir o progresso da sincronização.
+# --bwlimit=0.375M - Segue.: 10M link total, usar 3M do link total p/ o rclone. Então 3/8 = 0.375M
+rclone sync -P --bwlimit=0.375M /home/eduardo/Documentos/rclone/ dropbox_hw:arquivos/rclone
 exit
