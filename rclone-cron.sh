37559a77d669b004a7d047559c4aefcfcdf4d1af
Eduardo Campacci <campacc@gmail.com>
2019-02-19 09:38:44 -0300

    Incluido a flag --exclude no script

--- rclone-cron.sh
+++ rclone-cron.sh
@@ -6,5 +6,6 @@ exit 1
 fi
 # -P é para exibir o progresso da sincronização.
 # --bwlimit=0.375M - Segue.: 10M link total, usar 3M do link total p/ o rclone. Então 3/8 = 0.375M
-rclone sync -P --bwlimit=0.375M /home/eduardo/Documentos/rclone/ dropbox_hw:arquivos/rclone
+# --exclude "*.{pst,tar}" - Exclui da sincronização arquivos da extensão .pst e .tar.
+rclone sync -P --bwlimit=0.375M --exclude "*.{pst,tar}" /home/eduardo/Documentos/rclone/ dropbox_hw:arquivos/rclone
 exit
